234e4105e61ec9afb4dc4dc014fe2a8a42a6bc18
Jeong Min Lim <sogogibeef@gmail.com>
2023-01-07 16:13:24 -0500

    Update build files and do cursory clean up (#7)
    - Update build files 
    - Configure GitHub Action
    - Attempt to get tests running again

--- src/androidTest/java/trikita/obsqr/QrContentTest.java
+++ src/androidTest/java/trikita/obsqr/QrContentTest.java
@@ -29,96 +29,96 @@ public class QrContentTest {
 
         assertEquals("trikita.obsqr", appContext.getPackageName());
     }
+    @Test
+    public void testBrokenRegexp() {
+        String s = "http://qrs.ly/z24icxy";
+        assertFalse(s.matches(QrContent.GooglePlayContent.MATCH));
+        // For some reason, Moto G's String.match() caused ANR on this string
+        s = "https://play.google.com/store/apps/details?id=com.mvl.ThunderValley";
+        assertFalse(s.matches(QrContent.GooglePlayContent.MATCH));
+        assertTrue(s.matches(QrContent.WebUrlContent.MATCH));
+    }
 
-	public void testBrokenRegexp() {
-		String s = "http://qrs.ly/z24icxy";
-		assertFalse(s.matches(QrContent.GooglePlayContent.MATCH));
-		// For some reason, Moto G's String.match() caused ANR on this string
-		s = "https://play.google.com/store/apps/details?id=com.mvl.ThunderValley";
-		assertFalse(s.matches(QrContent.GooglePlayContent.MATCH));
-		assertTrue(s.matches(QrContent.WebUrlContent.MATCH));
-	}
+    public void testGooglePlayMatcher() {
+        for (Map.Entry<String, String> e : new HashMap<String, String>() {{
+            put("market://details?id=com.example", "com.example");
+            put("market://details?id=foo", "foo");
+            put("market://details.id.foo", "market://details.id.foo");
+            put("market://search?pub=trikita", "market://search?pub=trikita");
+            put("market://search?q=Some+Query&c=apps", "market://search?q=Some+Query&c=apps");
+            put("market://apps/collections/editors_choice", "market://apps/collections/editors_choice");
+        }}.entrySet()) {
+            QrContent qr = QrContent.from(getContext(), e.getKey());
+            assertEquals(QrContent.GooglePlayContent.class, qr.getClass());
+            assertEquals(e.getValue(), qr.content.toString());
+        }
+    }
 
-	public void testGooglePlayMatcher() {
-		for (Map.Entry<String, String> e : new HashMap<String, String>() {{
-			put("market://details?id=com.example", "com.example");
-			put("market://details?id=foo", "foo");
-			put("market://details.id.foo", "market://details.id.foo");
-			put("market://search?pub=trikita", "market://search?pub=trikita");
-			put("market://search?q=Some+Query&c=apps", "market://search?q=Some+Query&c=apps");
-			put("market://apps/collections/editors_choice", "market://apps/collections/editors_choice");
-		}}.entrySet()) {
-			QrContent qr = QrContent.from(getContext(), e.getKey());
-			assertEquals(QrContent.GooglePlayContent.class, qr.getClass());
-			assertEquals(e.getValue(), qr.content.toString());
-		}
-	}
+    public void testUrlMatcher() {
+        for (Map.Entry<String, String> e : new HashMap<String, String>() {{
+            put("http://example.com", "http://example.com");
+            put("https://example.com", "https://example.com");
+            put("ftp://example.com", null);
+            put("example.com", "http://example.com");
+            put("link to http://example.com", null);
+        }}.entrySet()) {
+            QrContent qr = QrContent.from(getContext(), e.getKey());
+            if (e.getValue() != null) {
+                assertEquals(QrContent.WebUrlContent.class, qr.getClass());
+                assertEquals(e.getValue(), qr.content.toString());
+            } else {
+                assertFalse(qr instanceof QrContent.WebUrlContent);
+            }
+        }
+    }
 
-	public void testUrlMatcher() {
-		for (Map.Entry<String, String> e : new HashMap<String, String>() {{
-			put("http://example.com", "http://example.com");
-			put("https://example.com", "https://example.com");
-			put("ftp://example.com", null);
-			put("example.com", "http://example.com");
-			put("link to http://example.com", null);
-		}}.entrySet()) {
-			QrContent qr = QrContent.from(getContext(), e.getKey());
-			if (e.getValue() != null) {
-				assertEquals(QrContent.WebUrlContent.class, qr.getClass());
-				assertEquals(e.getValue(), qr.content.toString());
-			} else {
-				assertFalse(qr instanceof QrContent.WebUrlContent);
-			}
-		}
-	}
+    public void testEmailMatcher() {
+        for (Map.Entry<String, String> e : new HashMap<String, String>() {{
+            put("mailto:johndoe@example.com", "johndoe@example.com");
+            put("MAILTO:johndoe@example.com", "johndoe@example.com");
+            put("MAILTO:johndoe@example.com?subject=Hello+world", "johndoe@example.com");
+        }}.entrySet()) {
+            QrContent qr = QrContent.from(getContext(), e.getKey());
+            if (e.getValue() != null) {
+                assertEquals(QrContent.EmailContent.class, qr.getClass());
+                assertEquals(e.getValue(), qr.content.toString());
+            } else {
+                assertFalse(qr instanceof QrContent.EmailContent);
+            }
+        }
+    }
 
-	public void testEmailMatcher() {
-		for (Map.Entry<String, String> e : new HashMap<String, String>() {{
-			put("mailto:johndoe@example.com", "johndoe@example.com");
-			put("MAILTO:johndoe@example.com", "johndoe@example.com");
-			put("MAILTO:johndoe@example.com?subject=Hello+world", "johndoe@example.com");
-		}}.entrySet()) {
-			QrContent qr = QrContent.from(getContext(), e.getKey());
-			if (e.getValue() != null) {
-				assertEquals(QrContent.EmailContent.class, qr.getClass());
-				assertEquals(e.getValue(), qr.content.toString());
-			} else {
-				assertFalse(qr instanceof QrContent.EmailContent);
-			}
-		}
-	}
+    public void testSmsMatcher() {
+        QrContent content;
+        content = QrContent.from(getContext(), "smsto:+123456789");
+        content = QrContent.from(getContext(), "smsto:+18554407400:I am interested in using Scanova");
+        // SMSTO
+        assertEquals(QrContent.SmsContent.class, content.getClass());
+    }
 
-	public void testSmsMatcher() {
-		QrContent content;
-		content = QrContent.from(getContext(), "smsto:+123456789");
-		content = QrContent.from(getContext(), "smsto:+18554407400:I am interested in using Scanova");
-		// SMSTO
-		assertEquals(QrContent.SmsContent.class, content.getClass());
-	}
+    public void testPhoneNumberMatcher() {
+        QrContent content;
+        content = QrContent.from(getContext(), "tel:+123456789");
+        // TEL:
+        assertTrue(content instanceof QrContent.PhoneNumberContent);
+    }
 
-	public void testPhoneNumberMatcher() {
-		QrContent content;
-		content = QrContent.from(getContext(), "tel:+123456789");
-		// TEL:
-		assertTrue(content instanceof QrContent.PhoneNumberContent);
-	}
+    public void testWifiMatcher() {
+        QrContent content;
+        content = QrContent.from(getContext(), "WIFI:S:Example;T:WPA;P:example123;;");
+        assertEquals(QrContent.WifiContent.class, content.getClass());
+    }
 
-	public void testWifiMatcher() {
-		QrContent content;
-		content = QrContent.from(getContext(), "WIFI:S:Example;T:WPA;P:example123;;");
-		assertEquals(QrContent.WifiContent.class, content.getClass());
-	}
+    public void testContactMatcher() {
+        QrContent content;
+        // TODO lots of other examples
+        content = QrContent.from(getContext(), "MECARD:N:John Doe;EMAIL:john@example.com;;");
+        assertEquals(QrContent.ContactContent.class, content.getClass());
+    }
 
-	public void testContactMatcher() {
-		QrContent content;
-		// TODO lots of other examples
-		content = QrContent.from(getContext(), "MECARD:N:John Doe;EMAIL:john@example.com;;");
-		assertEquals(QrContent.ContactContent.class, content.getClass());
-	}
-
-	public void testGeolocationMatcher() {
-		QrContent content;
-		content = QrContent.from(getContext(), "geo:0,0");
-		assertEquals(QrContent.GeoLocationContent.class, content.getClass());
-	}
+    public void testGeolocationMatcher() {
+        QrContent content;
+        content = QrContent.from(getContext(), "geo:0,0");
+        assertEquals(QrContent.GeoLocationContent.class, content.getClass());
+    }
 }
--- src/main/java/trikita/obsqr/MaterialButton.java
+++ src/main/java/trikita/obsqr/MaterialButton.java
@@ -13,92 +13,98 @@ import android.widget.TextView;
 
 public class MaterialButton extends TextView implements Animator.AnimatorListener {
 
-	private float mDownX;
-	private float mDownY;
+    private float mDownX;
+    private float mDownY;
 
-	private float mRadius;
-	private float mAlpha;
+    private float mRadius;
+    private float mAlpha;
 
-	private Paint mPaint;
+    private final Paint mPaint;
 
-	private ObjectAnimator mRadiusAnimator;
-	private ObjectAnimator mAlphaAnimator;
+    private ObjectAnimator mRadiusAnimator;
+    private ObjectAnimator mAlphaAnimator;
 
-	public MaterialButton(Context context) {
-		this(context, null);
-	}
+    private final Handler mHandler = new Handler();
 
-	public MaterialButton(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
+    public MaterialButton(Context context) {
+        this(context, null);
+    }
 
-	public MaterialButton(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		mPaint = new Paint();
-		mPaint.setColor(0x18000000);
-		setClickable(true);
-	}
+    public MaterialButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
-	@Override
-	public boolean onTouchEvent(final MotionEvent event) {
-		switch (event.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				mDownX = event.getX();
-				mDownY = event.getY();
-				mRadiusAnimator = ObjectAnimator.ofFloat(this, "radius", 0, getWidth()*1.5f);
-				mRadiusAnimator.setInterpolator(new AccelerateInterpolator());
-				mRadiusAnimator.setDuration(200);
-				mRadiusAnimator.start();
-				mAlpha = 0x18;
-				if (mAlphaAnimator != null) {
-					mAlphaAnimator.cancel();
-					mAlphaAnimator = null;
-				}
-				mRadiusAnimator.removeListener(this);
-				break;
-			case MotionEvent.ACTION_CANCEL:
-			case MotionEvent.ACTION_UP:
-				if (mRadiusAnimator.isRunning()) {
-					mRadiusAnimator.addListener(this);
-				} else {
-					this.onAnimationEnd(null);
-				}
-				break;
-		} 
-		return super.onTouchEvent(event);
-	}
+    public MaterialButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mPaint = new Paint();
+        mPaint.setColor(0x18000000);
+        setClickable(true);
+    }
 
-	public void onAnimationCancel(Animator animation) {}
-	public void onAnimationRepeat(Animator animation) {}
-	public void onAnimationStart(Animator animation) {}
-	public void onAnimationEnd(Animator animation) {
-		mAlphaAnimator = ObjectAnimator.ofFloat(this, "alpha", 0x18, 0).setDuration(200);
-		mAlphaAnimator.setInterpolator(new AccelerateInterpolator());
-		mAlphaAnimator.start();
-	}
+    @Override
+    public boolean onTouchEvent(final MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mDownX = event.getX();
+                mDownY = event.getY();
+                mRadiusAnimator = ObjectAnimator.ofFloat(this, "radius", 0, getWidth() * 1.5f);
+                mRadiusAnimator.setInterpolator(new AccelerateInterpolator());
+                mRadiusAnimator.setDuration(200);
+                mRadiusAnimator.start();
+                mAlpha = 0x18;
+                if (mAlphaAnimator != null) {
+                    mAlphaAnimator.cancel();
+                    mAlphaAnimator = null;
+                }
+                mRadiusAnimator.removeListener(this);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                if (mRadiusAnimator.isRunning()) {
+                    mRadiusAnimator.addListener(this);
+                } else {
+                    this.onAnimationEnd(null);
+                }
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
 
-	public void setAlpha(float alpha) {
-		mAlpha = alpha;
-		invalidate();
-	}
+    public void onAnimationCancel(Animator animation) {
+    }
 
-	public void setRadius(float radius) {
-		mRadius = radius;
-		invalidate();
-	}
+    public void onAnimationRepeat(Animator animation) {
+    }
 
-	@Override
-	protected void onDraw(final Canvas canvas) {
-		super.onDraw(canvas);
-		mPaint.setAlpha((int) mAlpha);
-		canvas.drawCircle(mDownX, mDownY, mRadius, mPaint);
-	}
+    public void onAnimationStart(Animator animation) {
+    }
 
-	private Handler mHandler = new Handler();
+    public void onAnimationEnd(Animator animation) {
+        mAlphaAnimator = ObjectAnimator.ofFloat(this, "alpha", 0x18, 0).setDuration(200);
+        mAlphaAnimator.setInterpolator(new AccelerateInterpolator());
+        mAlphaAnimator.start();
+    }
 
-	@Override
-	public boolean performClick() {
+    public void setAlpha(float alpha) {
+        mAlpha = alpha;
+        invalidate();
+    }
+
+    public void setRadius(float radius) {
+        mRadius = radius;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(final Canvas canvas) {
+        super.onDraw(canvas);
+        mPaint.setAlpha((int) mAlpha);
+        canvas.drawCircle(mDownX, mDownY, mRadius, mPaint);
+    }
+
+    @Override
+    public boolean performClick() {
         mHandler.postDelayed(MaterialButton.super::performClick, 200);
-		return true;
-	}
+        return true;
+    }
 }
